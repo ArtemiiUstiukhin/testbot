3999c126ad8e5f2efd004b1590a38cb995750a04
TemkaBanana <temes_matematik@mail.ru>
2018-09-09 15:20:55 +0300

    music get_id commit

--- bot.py
+++ bot.py
@@ -16,12 +16,6 @@ def find_file_ids(message):
         time.sleep(3)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
-� теперь отправим вслед за файлом его file_id
-            bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
-        time.sleep(3)
-
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
